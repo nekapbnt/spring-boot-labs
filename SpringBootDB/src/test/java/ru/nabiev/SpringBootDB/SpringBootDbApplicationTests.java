e9ad020a48c95323b5982df53e0e2276253e50b8
grrg <grigoriy.nabiev@gmail.com>
2024-10-28 15:55:10 +0500

    initial commit

---
+++ SpringBootDB/src/test/java/ru/nabiev/SpringBootDB/SpringBootDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nabiev.SpringBootDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
